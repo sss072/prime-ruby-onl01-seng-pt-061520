ec2c1c02306ba51f64e577bc6a2eacefa2d5e232
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-26 21:59:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry' 
+
+def prime?(integer)
+  
+  if integer < 0 or integer == 0 or integer == 1
+    return false
+
+     
+  
+  elsif integer > 1  
+    (2..integer - 1).to_a.each do |element|
+    return false if integer % element == 0 
+    end
+     
+    
+  end
+  true 
+end
